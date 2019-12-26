b641076fb500aebd32aabd725234d8238e15a542
Ethicalabhishek <51774982+Ethicalabhishek@users.noreply.github.com>
2019-12-26 14:00:46 +0530

    Add files via upload

---
+++ news.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="en-GB" dir="ltr">
+	<head>
+		<title>NEWS - ETHICALABHISHEK</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Abhishek Kumar" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 13.0.4.25 - www.websitex5.com" />
+		<meta property="og:locale" content="en" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://new.ethicalabhishek.xyz/news.php" />
+		<meta property="og:title" content="NEWS" />
+		<meta property="og:site_name" content="ETHICALABHISHEK" />
+		<meta property="og:image" content="http://new.ethicalabhishek.xyz/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="1280">
+		<meta property="og:image:height" content="960">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?13-0-4-25" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?13-0-4-25" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?13-0-4-25" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?13-0-4-25" media="screen" />
+		<link rel="stylesheet" type="text/css" href="style/menu.css?13-0-4-25" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/news.css?13-0-4-25-637129651013391483" media="screen" />
+		<script type="text/javascript" src="res/jquery.js?13-0-4-25"></script>
+		<script type="text/javascript" src="res/x5engine.js?13-0-4-25" data-files-version="13-0-4-25"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Your browser does not support the features necessary to display this website.','Your browser may not support the features necessary to display this website.','[1]Update your browser[/1] or [2]continue without updating[/2].','http://outdatedbrowser.com/'); };
+		</script>
+		<meta name="apple-itunes-app" content="app-id=665879631, app-argument=http://new.ethicalabhishek.xyz/x5feedready.xml">
+		<link rel="icon" href="favicon.png?13-0-4-25-637129651013361463" type="image/png" />
+		<link rel="alternate" type="application/rss+xml" title="ETHICALABHISHEK" href="x5feedready.php" />
+		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-124818591-1','auto');ga('send', 'pageview');</script>
+	</head>
+	<body>
+		<div id="imHeaderBg"></div>
+		<div id="imFooterBg"></div>
+		<div id="imPage">
+			<div id="imHeader">
+				<h1 class="imHidden">NEWS - ETHICALABHISHEK</h1>
+				<div id="imHeaderObjects"><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><a href="https://twitter.me/ethicalabhishek" onclick="return x5engine.utils.imPopUpWin('https://twitter.me/ethicalabhishek','imPopUp', 800,600);"><img id="imHeader_imObjectImage_03" src="images/White_twitter.png" title="" alt="" /></a></div></div></div><div id="imHeader_imObjectImage_04_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_4" class="" > <div id="imHeader_imCellStyleGraphics_4"></div><div id="imHeader_imCellStyle_4" ><a href="https://facebook.com/ethicalabhishek" rel="nofollow" onclick="return x5engine.utils.imPopUpWin('https://facebook.com/ethicalabhishek','imPopUp', 800,600);"><img id="imHeader_imObjectImage_04" src="images/White_facebook.png" title="" alt="" /></a></div></div></div><div id="imHeader_imTextObject_05_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_5" class="" > <div id="imHeader_imCellStyleGraphics_5"></div><div id="imHeader_imCellStyle_5" ><div id="imHeader_imTextObject_05">
+	<div class="text-tab-content"  id="imHeader_imTextObject_05_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><span class="fs8 cf1">+919931607942</span></div><div style="text-align: center;"><span class="fs8 cf1">NOREPLY@ETHICALABHISHEK,XYZ</span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imHeader_imObjectImage_06_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_6" class="" > <div id="imHeader_imCellStyleGraphics_6"></div><div id="imHeader_imCellStyle_6" ><img id="imHeader_imObjectImage_06" src="images/youtube_orig.png" title="" alt="" /></div></div></div></div>
+			</div>
+			<a class="imHidden" href="#imGoToCont" title="Skip the main menu">Go to content</a>
+			<a id="imGoToMenu"></a><p class="imHidden">Main menu:</p>
+			<div id="imMnMnContainer">
+				<div id="imMnMnGraphics"></div>
+				<div id="imMnMn" class="auto main-menu">
+					<div class="hamburger-site-background menu-mobile-hidden"></div><div class="hamburger-button"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container"><div class="hamburger-menu-background menu-mobile-hidden"><div class="hamburger-menu-close-button"><span>&times;</span></div></div></div>
+				<ul class="auto menu-mobile-hidden">
+					<li id="imMnMnNode0" class=" imPage">
+						<a href="index.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Home Page</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode6" class="imMnMnSeparator filled-separator">
+						<span class="imMnMnFirstBg">
+							<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Separator 1</span></span>
+						</span>
+					</li><li id="imMnMnNode3" class="imPage imMnMnCurrent">
+						<a href="news.php">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">NEWS</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode4" class=" imPage">
+						<a href="articles.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">ARTICLES</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode5" class=" imPage">
+						<a href="ethicalabhishk.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">ETHICALABHISHK</span></span>
+							</span>
+						</a>
+					</li></ul>
+				</div>
+			</div>
+			<div id="imContentContainer">
+				<div id="imContentGraphics"></div>
+				<div id="imContent">
+					<a id="imGoToCont"></a>
+				<div id="imPageRow_1" class="imPageRow">
+				
+				<div id="imPageRowContent_1" class="imContentDataContainer">
+				<div id="imCell_1" class="" > <div id="imCellStyleGraphics_1"><div style="position: relative; height: 100%;"><div id="imCellStyleGraphicsC_1"></div><div id="imCellStyleGraphicsT_1"></div><div id="imCellStyleGraphicsTL_1"></div><div id="imCellStyleGraphicsTR_1"></div><div id="imCellStyleGraphicsL_1"></div><div id="imCellStyleGraphicsR_1"></div><div id="imCellStyleGraphicsB_1"></div><div id="imCellStyleGraphicsBL_1"></div><div id="imCellStyleGraphicsBR_1"></div></div></div><div id="imCellStyle_1"  data-responsive-sequence-number="1"><div id="imTextObject_3_01">
+					<div class="text-tab-content"  id="imTextObject_3_01_tab0" style="">
+						<div class="text-inner">
+							<div><div><div>घटनाक्रम</div><div><br></div><div>1933 30 जनवरी - एडॉल्फ हिटलर जर्मनी के चांसलर बने। उनकी नाजी पार्टी, या तीसरा रैह, सत्ता लेता है और हिटलर अनिवार्य रूप से जर्मनी का तानाशाह है।</div><div><br></div><div>1936 25 अक्टूबर - नाजी जर्मनी और फासीवादी इटली ने रोम-बर्लिन धुरी संधि का गठन किया।</div><div><br></div><div>1936 25 नवंबर - नाजी जर्मनी और इंपीरियल जापान एंटी-कॉमिन्टर्न संधि पर हस्ताक्षर करते हैं। यह साम्यवाद और रूस के खिलाफ एक समझौता था।</div><div><br></div><div>1937 जुलाई 7 - जापान ने चीन पर हमला किया।</div><div><br></div><div>1938 12 मार्च - हिटलर ने ऑस्ट्रिया देश को जर्मनी में बदल दिया। इसे आन्क्स्लस भी कहा जाता है।</div><div><br></div><div>द्वितीय विश्व युद्ध</div><div><br></div><div>1939 1 सितंबर - जर्मनी ने पोलैंड पर हमला किया। द्वितीय विश्व युद्ध शुरू होता है।</div><div><br></div><div>1939 3 सितंबर - फ्रांस और ग्रेट ब्रिटेन ने जर्मनी के खिलाफ युद्ध की घोषणा की।</div><div><br></div><div>1940 9 अप्रैल से 9 जून - जर्मनी ने डेनमार्क और नॉर्वे पर आक्रमण किया और नियंत्रण किया।</div><div><br></div><div>1940 मई 10 से 22 जून - जर्मनी ने नीदरलैंड, बेल्जियम और उत्तरी फ्रांस सहित पश्चिमी यूरोप के अधिकांश हिस्सों पर कब्जा करने के लिए ब्लिट्जक्रेग, जिसका अर्थ है बिजली की लड़ाई, का इस्तेमाल किया।</div><div><br></div><div>1940 मई 30 - विंस्टन चर्चिल ब्रिटिश सरकार के नेता बने।</div><div><br></div><div>1940 10 जून - इटली ने एक्सिस शक्तियों के सदस्य के रूप में युद्ध में प्रवेश किया।</div><div><br></div><div>1940 जुलाई 10 - जर्मनी ने ग्रेट ब्रिटेन पर हवाई हमला किया। ये हमले अक्टूबर के अंत तक चले और ब्रिटेन की लड़ाई के रूप में जाना जाता है।</div><div><br></div><div>1940 22 सितंबर - जर्मनी, इटली और जापान ने त्रिपक्षीय संधि पर हस्ताक्षर करते हुए एक्सिस एलायंस का निर्माण किया।</div><div><br></div><div>1941 जून 22 - जर्मनी और एक्सिस पॉवर्स ने चार मिलियन से अधिक सैनिकों की एक बड़ी संख्या के साथ रूस पर हमला किया।</div><div><br></div><div>1941 दिसंबर 7 - जापानी ने पर्ल हार्बर में अमेरिकी नौसेना पर हमला किया। अगले दिन अमेरिका मित्र राष्ट्रों की ओर से द्वितीय विश्व युद्ध में प्रवेश करता है।</div><div><br></div><div>1942 4 जून - अमेरिकी नौसेना ने मिडवे की लड़ाई में जापानी नौसेना को हराया।</div><div><br></div><div>1942 जुलाई 10 - मित्र राष्ट्रों ने आक्रमण किया और सिसिली द्वीप ले लिया।</div><div><br></div><div>1943 सितंबर 3 - इटली मित्र राष्ट्रों के सामने आत्मसमर्पण करता है, हालांकि जर्मनी मुसोलिनी को उत्तरी इटली में सरकार से बचने और स्थापित करने में मदद करता है।</div><div><br></div><div>1944 6 जून - डी-डे और नॉरमैंडी आक्रमण। मित्र देशों की सेना ने फ्रांस पर आक्रमण किया और जर्मनों को पीछे धकेल दिया।</div><div><br></div><div>1944 25 अगस्त - पेरिस जर्मन नियंत्रण से मुक्त हुआ।</div><div><br></div><div>1944 16 दिसंबर - जर्मनों ने बुल्गे की लड़ाई में एक बड़ा हमला किया। वे जर्मन सेना के भाग्य को सील करने वाले मित्र राष्ट्रों से हार जाते हैं।</div><div><br></div><div>1945 फरवरी 19 - यूएस मरीन ने इवो जीमा द्वीप पर आक्रमण किया। एक भयंकर युद्ध के बाद वे द्वीप पर कब्जा कर लेते हैं।</div><div><br></div><div>1945 12 अप्रैल - अमेरिकी राष्ट्रपति फ्रैंकलिन रूजवेल्ट का निधन। वह राष्ट्रपति हैरी ट्रूमैन द्वारा सफल होता है।</div><div><br></div><div>1945 मार्च 22 - जनरल पैटन के तहत अमेरिका की तीसरी सेना राइन नदी को पार करती है।</div><div><br></div><div>1945 30 अप्रैल - एडोल्फ हिटलर ने आत्महत्या की क्योंकि वह जानता है कि जर्मनी युद्ध हार चुका है।</div><div><br></div><div>1945 मई 7 - जर्मनी ने मित्र राष्ट्रों के सामने आत्मसमर्पण किया।</div><div><br></div><div>1945 6 अगस्त - अमेरिका ने जापान के हिरोशिमा पर परमाणु बम गिराया। शहर तबाह हो गया है।</div><div><br></div><div>1945 9 अगस्त - नागासाकी, जापान पर एक और परमाणु बम गिराया गया।</div><div><br></div><div>1945 सितंबर 2 - जापान ने अमेरिकी जनरल डगलस मैकआर्थर और मित्र राष्ट्रों के सामने आत्मसमर्पण किया।</div></div></div>
+						</div>
+					</div>
+				
+				</div>
+				</div></div></div>
+				</div>
+				
+				<div id="imFooPad"></div><div id="imBtMnContainer" class="imContentDataContainer"><div id="imBtMn"><a href="index.html">Home Page</a> | <a href="news.php">NEWS</a> | <a href="articles.html">ARTICLES</a> | <a href="ethicalabhishk.html">ETHICALABHISHK</a> | <a href="imsitemap.html">General Site Map</a></div></div>
+					<div class="imClear"></div>
+				</div>
+			</div>
+			<div id="imFooter">
+				<div id="imFooterObjects"><div id="imFooter_imObjectSearch_01_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_1" class="" > <div id="imFooter_imCellStyleGraphics_1"></div><div id="imFooter_imCellStyle_1" ><div id="imFooter_imObjectSearch_01"><form id="imFooter_imObjectSearch_01_form" action="imsearch.php" method="get"><fieldset><input type="text" id="imFooter_imObjectSearch_01_field" name="search" value="" /><button id="imFooter_imObjectSearch_01_button">SEARCH</button></fieldset></form><script>$('#imFooter_imObjectSearch_01_button').click(function() { $('#imFooter_imObjectSearch_01_form').submit(); return false; });</script></div></div></div></div></div>
+			</div>
+		</div>
+		<span class="imHidden"><a href="#imGoToCont" title="Read this page again">Back to content</a> | <a href="#imGoToMenu" title="Remain on this page">Back to main menu</a></span>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">To use this website you must enable JavaScript.</div></noscript>
+	</body>
+</html>
